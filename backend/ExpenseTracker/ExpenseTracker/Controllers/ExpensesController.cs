5b7f5fa217af5ea56bf7601a53c5de9f61f46346
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2025-10-16 01:16:03 +0530

    get expenses

---
+++ backend/ExpenseTracker/ExpenseTracker/Controllers/ExpensesController.cs
@@ -0,0 +1,35 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly DataContext _dbContext;
+
+        public ExpensesController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Expense>>> GetAllExpenses()
+        {
+            var expenses = await _dbContext.Expenses.ToListAsync();
+            /*var expenses = new List<Expense> {
+                new Expense{
+                    Id = 1,
+                    ExpenseName = "Test",
+                    Description = "Test Des",
+                    ExpenseValue = 123,
+                }
+            };*/
+            return Ok(expenses);
+        }
+    }
+}
---
+++ backend/ExpenseTracker/ExpenseTracker/Entities/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTracker.Entities
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public required string ExpenseName { get; set; }
+        public string? Description { get; set; }
+        public required float ExpenseValue { get; set; }
+    }
+}

b1884999177f0662b53cbfbdf7709966971c4d0d
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2025-10-16 01:46:53 +0530

    crud operations

--- backend/ExpenseTracker/ExpenseTracker/Controllers/ExpensesController.cs
+++ backend/ExpenseTracker/ExpenseTracker/Controllers/ExpensesController.cs
@@ -31,5 +31,54 @@ namespace ExpenseTracker.Controllers
             };*/
             return Ok(expenses);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Expense>> GetExpenseById(int id)
+        {
+            var expense = await _dbContext.Expenses.FindAsync(id);
+            
+            if(expense == null) return NotFound("Expense not found.");
+
+            return Ok(expense);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Expense>>> AddExpense(Expense expense)
+        {
+             _dbContext.Expenses.Add(expense);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(await _dbContext.Expenses.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Expense>>> UpdateExpense(Expense updatedExpense)
+        {
+            var expense = await _dbContext.Expenses.FindAsync(updatedExpense.Id);
+
+            if (expense == null) return NotFound("Expense not found.");
+
+            expense.ExpenseName = updatedExpense.ExpenseName;
+            expense.Description = updatedExpense.Description;
+            expense.ExpenseValue = updatedExpense.ExpenseValue;
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(await _dbContext.Expenses.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Expense>> DeleteExpenseById(int id)
+        {
+            var expense = await _dbContext.Expenses.FindAsync(id);
+
+            if (expense == null) return NotFound("Expense not found.");
+
+            _dbContext.Expenses.Remove(expense);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(await _dbContext.Expenses.ToListAsync());
+        }
     }
 }

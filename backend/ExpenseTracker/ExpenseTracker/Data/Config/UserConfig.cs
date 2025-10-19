691ae41e4786485e7dcb5c6bd4f792b0e02c01cd
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2025-10-19 22:58:26 +0530

    entityType config file

---
+++ backend/ExpenseTracker/ExpenseTracker/Data/Config/UserConfig.cs
@@ -0,0 +1,38 @@
+﻿using ExpenseTracker.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpenseTracker.Data.Config
+{
+    public class UserConfig : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            builder.Property(n => n.Name).IsRequired().HasMaxLength(250);
+            builder.Property(n => n.Email).IsRequired().HasMaxLength(250);
+
+            builder.HasData(new List<User>()
+            {
+                new User
+                {
+                    Id = 1,
+                    Name = "Test Name",
+                    Email = "Test Email",
+                    Password = "Test PW",
+                },
+                new User
+                {
+                    Id = 2,
+                    Name = "Test Name 2",
+                    Email = "Test Email 2",
+                    Password = "Test PW 2",
+                }
+            });
+        }
+    }
+}
--- backend/ExpenseTracker/ExpenseTracker/Data/DatabaseContext.cs
+++ backend/ExpenseTracker/ExpenseTracker/Data/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using ExpenseTracker.Entities;
+﻿using ExpenseTracker.Data.Config;
+using ExpenseTracker.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseTracker.Data
@@ -13,29 +14,8 @@ namespace ExpenseTracker.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>().HasData(new List<User>()
-            {
-                new User
-                {
-                    Id = 1,
-                    Name = "Test Name",
-                    Email = "Test Email",
-                    Password = "Test PW",
-                },
-                new User
-                {
-                    Id = 2,
-                    Name = "Test Name 2",
-                    Email = "Test Email 2",
-                    Password = "Test PW 2",
-                }
-            });
+            modelBuilder.ApplyConfiguration(new UserConfig()); 
 
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.Property(n => n.Name).IsRequired().HasMaxLength(250);
-                entity.Property(n => n.Email).IsRequired().HasMaxLength(250);
-            });
         }
     }
 }

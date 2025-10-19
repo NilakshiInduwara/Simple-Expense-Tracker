cea7d2f3dbeb0ac588955db4fdea39192f79650e
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2025-10-19 16:07:38 +0530

    insert data into user table

--- backend/ExpenseTracker/ExpenseTracker/Data/DatabaseContext.cs
+++ backend/ExpenseTracker/ExpenseTracker/Data/DatabaseContext.cs
@@ -10,5 +10,26 @@ namespace ExpenseTracker.Data
             
         }
         DbSet<User> users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new List<User>()
+            {
+                new User
+                {
+                    Id = 1,
+                    Name = "Test Name",
+                    Email = "Test Email",
+                    Password = "Test PW",
+                },
+                new User
+                {
+                    Id = 2,
+                    Name = "Test Name 2",
+                    Email = "Test Email 2",
+                    Password = "Test PW 2",
+                }
+            });
+        }
     }
 }
---
+++ backend/ExpenseTracker/ExpenseTracker/Migrations/20251019103440_InsertDataIntoUserTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ExpenseTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertDataIntoUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "Id", "Email", "Name", "Password" },
+                values: new object[,]
+                {
+                    { 1, "Test Email", "Test Name", "Test PW" },
+                    { 2, "Test Email 2", "Test Name 2", "Test PW 2" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
